5de9a5f26c94c26ab4ce534e2f7d07214beb1e88
Luke Granger <grangerl330@gmail.com>
2019-01-12 17:20:27 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
-    word.chars.collect do |letter|
+    word.chars.collect do |letter| #.chars returns an array of letters in a string 
       esperanto_alphabet.index(letter)
     end
   end 
