2bbc631f5d75d8ff1f259d041d7528fa66f7c1fb
Luke Granger <grangerl330@gmail.com>
2019-01-12 17:28:19 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,5 +7,7 @@ def alphabetize(arr)
   end 
 end
 
-
-#.chars returns an array of characters in a string 
+#Create variable, esperanto_alphabet, with value of the esperanto alpabet as a string 
+#Use .sort_by method on the given array, which will sort the elements in the array by the conditions in the block. .sort_by yields each word from the array when iterating
+#Use .chars on the yielded word, which returns an array of characters in the string, then iterate through that array with .collect, yielding each letter from the word. (The array returned from .chars will contain each letter from the word as a separate element)
+#esperanto_alphabet.index(letter) is the condition that it will sort_by. This will essentially check what index the letter being yielded is in the esperanto_alphabet string, and sort. Lower indexes will be placed first 
