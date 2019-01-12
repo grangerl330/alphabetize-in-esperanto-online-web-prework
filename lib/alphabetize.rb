8085528b396a4f566d9fe5bd51646e515e9c4486
Luke Granger <grangerl330@gmail.com>
2019-01-12 17:18:05 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,6 +3,7 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
     word.chars.each do |letter|
-      
-
+      esperanto_alphabet.index(letter)
+    end
+  end 
 end
